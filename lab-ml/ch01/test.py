cd2868e1a12e2a9bffecdb99784319721c12448e
JakeOh <jake.oh.lecture@gmail.com>
2020-06-04 18:03:14 +0900

    Add files via upload

---
+++ lab-ml/ch01/test.py
@@ -0,0 +1,11 @@
+import matplotlib
+import numpy
+import pandas
+import seaborn
+import sklearn
+
+print('matplotlib verion:', matplotlib.__version__)
+print('numpy version:', numpy.__version__)
+print('pandas version:', pandas.__version__)
+print('seaborn version:', seaborn.__version__)
+print('scikit-learn version:', sklearn.__version__)
